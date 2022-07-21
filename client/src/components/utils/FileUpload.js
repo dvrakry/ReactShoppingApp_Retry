b00a70afeb25245cd9e608df7dd710c5802f16c9
dvrakry <dv.rakry@gmail.com>
2022-07-21 13:19:00 +0900

    2022-07-21

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Dropzone from "react-dropzone";
 import { Icon } from "antd";
 import Axios from "axios";
 
-function FileUpload() {
+function FileUpload(props) {
+  const [Images, setImages] = useState([]);
+
   const dropHandler = (files) => {
     let formData = new FormData();
 
@@ -14,10 +16,23 @@ function FileUpload() {
 
     Axios.post("/api/product/image", formData, config).then((response) => {
       if (response.data.success) {
+        setImages([...Images, response.data.filePath]);
+        //부모에 변경한 이미지 정보 보내기위해
+        props.refreshFunction([...Images, response.data.filePath]);
       } else {
         alert("Failed to save images");
       }
     });
+  };
+
+  const deleteHandler = (image) => {
+    const currenIndex = Images.indexOf(image);
+
+    let newImages = [...Images];
+    newImages.splice(currenIndex, 1);
+
+    setImages(newImages);
+    props.refreshFunction(newImages);
   };
 
   return (
@@ -42,6 +57,15 @@ function FileUpload() {
           </section>
         )}
       </Dropzone>
+
+      {Images.map((image, index) => (
+        <div onClick={() => deleteHandler(image)} key={index}>
+          <img
+            style={{ display: "flex", width: "350px", height: "240px" }}
+            src={`http://localhost:5000/${image}`}
+          />
+        </div>
+      ))}
     </div>
   );
 }
